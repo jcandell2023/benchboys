e6221ede884edac455168e7ee15b69039064c0ae
Jeff Candell <jcandell@wustl.edu>
2023-08-13 18:42:24 -0400

    add rankings page

---
+++ src/routes/rankings/+page.svelte
@@ -0,0 +1,26 @@
+<table class="table is-hoverable">
+    <thead>
+        <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Position</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>Jeff Candell</td>
+            <td>WR</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Peter Huff</td>
+            <td>QB</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Arun Movva</td>
+            <td>RB</td>
+        </tr>
+    </tbody>
+</table>
