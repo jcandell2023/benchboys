a3d987ac7df243a09edb16a8a5142de35f23ebe6
Jeff Candell <jcandell@wustl.edu>
2023-08-13 17:55:01 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import '../app.scss'
+
+    let active = false
+
+    function toggleBurger(event:MouseEvent){
+      event.preventDefault()
+      active = !active
+    }
+</script>
+
+<nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="/">Bench Boys</a>
+        <a
+            role="button"
+            class="navbar-burger {active && 'is-active'}"
+            aria-label="menu"
+            aria-expanded={active}
+            data-target="navbarItems"
+            on:click={toggleBurger}
+        >
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+        </a>
+    </div>
+    <div id="navbarItems" class="navbar-menu {active && 'is-active'}">
+        <div class="navbar-start">
+            <a class="navbar-item" href="/"> Home </a>
+            <a class="navbar-item" href="/articles"> Articles </a>
+            <a class='navbar-item' href='/about'>About Us</a>
+        </div>
+    </div>
+</nav>
+
+<div class="container">
+    <slot />
+</div>
